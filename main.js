dcb0a0ac9726915487168ff1c0764b365a79b949
Eniola Johnson <writingincodes@gmail.com>
2020-12-12 03:22:29 +0100

    Image rotates with CSS classes when keydown event occurs

---
+++ main.js
@@ -0,0 +1,28 @@
+var car = document.querySelector('img');
+
+var carDirectionData = {
+  north: 'north',
+  south: 'south',
+  west: 'west',
+  east: 'east'
+}
+
+function turnCar(event) {
+
+  if (event.code === "ArrowUp") {
+    car.classList.add('north');
+  }else if (event.code === "ArrowDown") {
+    car.classList.add('south');
+  }else if (event.code === "ArrowLeft") {
+    car.classList.add('west');
+  }else if (event.code === "ArrowRight") {
+    car.classList.add("east");
+  }else{
+    return;
+  }
+  console.log(event);
+}
+
+
+document.addEventListener('keydown', turnCar);
+// ArrowRight ArrowLeft ArrowDown ShiftRight

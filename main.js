83f65d39667d8ec9d2d28ed3329357fcc9e2bdfb
Eniola Johnson <writingincodes@gmail.com>
2020-12-13 14:04:02 +0100

    Set the direction in the data model property dynamically

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 var car = document.querySelector('img');
 
 var carDirectionData = {
-  directionTop: 'north',
-  directionBottom: 'south',
-  directionLeft: 'west',
-  directionRight: 'east'
+  direction: 'east'
 }
 
 function turnCar(event) {
   if (event.code === "ArrowUp") {
     car.className = 'north';
+    carDirectionData.direction = 'north';
   } else if (event.code === "ArrowDown") {
     car.className = 'south';
+    carDirectionData.direction = 'south';
   } else if (event.code === "ArrowLeft") {
     car.className = 'west';
+    carDirectionData.direction = 'west';
   } else if (event.code === "ArrowRight") {
     car.className = "east";
+    carDirectionData.direction = 'east';
   }
 }
 

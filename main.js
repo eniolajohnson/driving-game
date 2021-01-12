004920c054d2cd727b5561bcd6d392ad53f38a97
Eniola Johnson <writingincodes@gmail.com>
2021-01-12 11:19:29 +0100

    User steers car with the arrow buttons on keyboard

--- main.js
+++ main.js
@@ -9,9 +9,13 @@ var carDirectionData = {
   start: false
 }
 
-var left = 0;
+var left = 8;
+var topNum = 8;
 
 var intervalId = null;
+var intervalId2 = null;
+var intervalId3 = null;
+var intervalId4 = null;
 
 function carMove(e) {
   if (e.code === "ArrowUp") {
@@ -32,17 +36,63 @@ function carMove(e) {
 document.addEventListener('keydown', carMove);
 
 function playGame(e){
-
   if (e.code === "Space" && carDirectionData.start === false) {
      intervalId = setInterval(() => {
       carDirectionData.start = true;
       left += 16;
       car.style.left = left + 'px';
       carDirectionData.location.x = left;
-    }, 30);
+    }, 50);
   } else if (e.code === 'Space' && carDirectionData.start === true){
     clearInterval(intervalId);
+    clearInterval(intervalId2);
+    clearInterval(intervalId3);
+    clearInterval(intervalId4);
     carDirectionData.start = false;
+  }
+  if (e.code === 'ArrowDown') {
+    clearInterval(intervalId);
+    clearInterval(intervalId4);
+    clearInterval(intervalId3);
+    intervalId2 = setInterval(() => {
+      carDirectionData.start = true;
+      topNum += 16;
+      car.style.top = topNum + 'px';
+      carDirectionData.location.y = topNum;
+    }, 50);
+  }
+  if (e.code === 'ArrowRight') {
+    clearInterval(intervalId4);
+    clearInterval(intervalId2);
+    clearInterval(intervalId3);
+    intervalId = setInterval(() => {
+      carDirectionData.start = true;
+      left += 16;
+      car.style.left = left + 'px';
+      carDirectionData.location.x = left;
+    }, 50);
+  }
+  if (e.code === 'ArrowUp') {
+    clearInterval(intervalId);
+    clearInterval(intervalId2);
+    clearInterval(intervalId4);
+    intervalId3 = setInterval(() => {
+      carDirectionData.start = true;
+      topNum-= 16;
+      car.style.top = topNum + 'px';
+      carDirectionData.location.y = topNum;
+    }, 50);
+  }
+  if (e.code === 'ArrowLeft') {
+    clearInterval(intervalId);
+    clearInterval(intervalId2);
+    clearInterval(intervalId3);
+    intervalId4 = setInterval(() => {
+      carDirectionData.start = true;
+      left -= 16;
+      car.style.left = left + 'px';
+      carDirectionData.location.x = left;
+    }, 50);
   }
 }
 
